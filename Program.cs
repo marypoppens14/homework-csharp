f874928e1d3e90c8cef3a42ea565f87ee0dda686
Maria Popova <marypoppens14@gmail.com>
2023-04-28 11:51:35 +0300

    task 3

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// на вход принимает число 
+// выдаёт, является ли оно чётным
+
+string a = Console.ReadLine();
+int b = Convert.ToInt32(a);
+
+if (b % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+
+else Console.WriteLine("нет");

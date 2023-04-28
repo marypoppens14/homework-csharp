65d64f1ead9e468d321e8ecb45ac95f4cf5ce403
Maria Popova <marypoppens14@gmail.com>
2023-04-28 11:11:04 +0300

    task 2

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// на вход принимает три числа 
+// выдаёт максимальное из этих чисел
+
+string a = Console.ReadLine();
+string b = Console.ReadLine();
+string c = Console.ReadLine();
+
+int d = Convert.ToInt32(a);
+int e = Convert.ToInt32(b);
+int f = Convert.ToInt32(c);
+
+int max = d;
+
+if (d > max) max = d;
+if (e > max) max = e;
+if (f > max) max = f;
+
+Console.Write("max = ");
+Console.WriteLine(max);

bf3a0a1c9572f74d406810658863ff9c934bdc75
Maria Popova <marypoppens14@gmail.com>
2023-04-28 12:29:42 +0300

    task 4

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// на вход принимает число (N)
+// на выходе показывает все чётные числа от 1 до N
+
+string N = Console.ReadLine();
+int x = Convert.ToInt32(N);
+int y = 2;
+
+while (x > y)
+
+{
+    if(x % 2 == 0)
+    Console.Write(y + " ");
+    y +=2;
+     
+}
+Console.WriteLine("Чётные числа: ");

d211b3657f9300e0cb9d675c2d133e19190e1742
Maria Popova <marypoppens14@gmail.com>
2023-04-28 08:49:07 +0300

    task 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// на вход принимает два числа 
+// выдаёт, какое число большее, а какое меньшее
+
+string a = Console.ReadLine();
+string b = Console.ReadLine();
+int c = Convert.ToInt32(a);
+int d = Convert.ToInt32(b);
+
+if(c > d)
+{
+    Console.WriteLine(c);
+}
+else Console.WriteLine(d);
